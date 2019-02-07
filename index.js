4c5bd5df6f8414cea00314afa0958f09d94b3e63
Paul Malone <Paul.Malone@version1.com>
2019-02-07 10:10:51 +0000

    Commented out console log when index.js is running

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ app.get('/',(req,res)=>{
   res.sendFile(__dirname + '/index.html');
 });
 app.listen(3001);
-console.log("Server listening on port 3001");
+//console.log("Server listening on port 3001");
